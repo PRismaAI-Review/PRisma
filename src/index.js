b8ca6a1ee435de20bff3f45479a67bbd2e9425ff
misstry <trupti.nayak@redspace.com>
2025-05-14 16:01:00 -0300

    Test commit for PRisma Bot

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-require('dotenv').config({ path: './config.env' });
+require('dotenv').config();
+
 console.log('Environment variables loaded:');
 console.log('GITHUB_TOKEN:', process.env.GITHUB_TOKEN ? 'Set' : 'Not set');
 console.log('GEMINI_API_KEY:', process.env.GEMINI_API_KEY ? 'Set' : 'Not set');

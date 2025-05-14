041b5849918f9090d141f09cb21faf518f8e2b9a
misstry <trupti.nayak@redspace.com>
2025-05-14 14:46:37 -0300

    comments removed

--- src/index.js
+++ src/index.js
@@ -46,11 +46,4 @@ app.listen(PORT, () => {
   console.log(`PRisma server running on port ${PORT}`);
 });
 // Test webhook
-// Test webhook
-// Test comment for PRisma webhook
-// Test webhook
-// Test webhook
-// Test webhook
-// Test webhook
-// Test webhook
-// Test webhook
+
